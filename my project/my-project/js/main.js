6de95b9e4138034016b6b9d3f6004a71ab7d6fe9
VladNek <91415904+VladNek@users.noreply.github.com>
2023-08-14 17:47:30 +0300

    add: animation

--- my project/my-project/js/main.js
+++ my project/my-project/js/main.js
@@ -42,6 +42,9 @@ $(document).ready(function () {
 
   next.css('left', prev.width() + 10 + bullets.width() + 10)
   bullets.css('left', prev.width() + 10)
+
+  new WOW().init();
+
 });
 
 $(function(){
