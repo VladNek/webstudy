48f2da6d01ec607061c26c0734a4e38a13f0b83a
VladNek <91415904+VladNek@users.noreply.github.com>
2023-08-22 11:04:53 +0300

    add: icon image on yandex map

---
+++ my project/my project/my-project/gulpfile.js
@@ -0,0 +1,33 @@
+const {src,dest,watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+
+
+
+// Static server
+ function bs() {
+ serveSass();
+ browserSync.init({
+     server: {
+         baseDir: "./"
+     }
+ });
+ watch("./*.html").on('change', browserSync.reload);
+ watch("./sass/**/*.sass", serveSass);
+ watch("./sass/**/*.scss", serveSass);
+ watch("./js/*.js").on('change', browserSync.reload);
+};
+
+ function serveSass() {
+    return src("./sass/**/*.sass", "./sass/**/*.sass")
+        .pipe(sass())
+        .pipe(autoprefixer({
+			cascade: false
+		}))
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+};
+
+
+exports.serve = bs;
